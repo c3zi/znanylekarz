62d3e078897979a0f5bfaf38e7691efdb7f8e81a
Przemyslaw Furtak <przemyslawf@gmail.com>
2015-06-24 23:10:41 +0200

    Exception when user does not exist.

---
+++ src/AppBundle/Model/Exception/UserDoesNotExist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: c3zi
+ * Date: 24/06/15
+ * Time: 23:07
+ */
+
+namespace AppBundle\Model\Exception;
+
+
+class UserDoesNotExist extends \Exception
+{
+
+}
