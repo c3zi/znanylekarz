f6aec603f5e7bfed8fcd6a609ea2d345767dde0c
Przemyslaw Furtak <przemyslawf@gmail.com>
2015-06-25 15:10:41 +0200

    Remove UserManager (instead of this Handler is used)

---
+++ src/AppBundle/Exception/InvalidFormException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: c3zi
+ * Date: 25/06/15
+ * Time: 10:48
+ */
+
+namespace AppBundle\Exception;
+
+
+class InvalidFormException
+{
+
+}
---
+++ src/AppBundle/Validator/Token.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: c3zi
+ * Date: 25/06/15
+ * Time: 06:54
+ */
+
+namespace AppBundle\Validator;
+
+
+class Token
+{
+
+}
